ac6b7ff3daf11371bde9ffad3a30e0498edaf946
rob.jordan054@gmail.com <rob.jordan054@gmail.com>
2023-04-12 22:02:55 -0400

    JavaScript Variable

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
-
-// Exercise 2
+// Exercise 1 & Exercise 2
+const firstName = "Eddie";
+let lastName = "Vedder";
+var age = 61;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Excercise 5
+let title = "Name of the Wind";
+let authoer = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
